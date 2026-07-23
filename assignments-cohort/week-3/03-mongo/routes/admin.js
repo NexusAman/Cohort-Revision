336b39c9f4638ea53fbb380c9f63fe66f49265b1
NexusAman <amankumarkeshri1@gmail.com>
2026-07-23 18:30:09 +0530

    solved 03-mongo assignment

--- assignments-cohort/week-3/03-mongo/routes/admin.js
+++ assignments-cohort/week-3/03-mongo/routes/admin.js
@@ -1,18 +1,69 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  try {
+    const { username, password } = req.body;
+
+    const existingAdmin = await Admin.findOne({ username });
+
+    if (existingAdmin) {
+      return res.status(409).send("Admin already exists");
+    }
+
+    await Admin.create({
+      username: username,
+      password: password,
+    });
+
+    return res.status(201).send("Admin created successfully!");
+  } catch (err) {
+    return res.status(500).send("Failed to create admin");
+  }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+  try {
+    const { title, description, imageLink, price } = req.body;
+
+    const existingCourse = await Course.findOne({
+      title,
+    });
+
+    if (existingCourse) {
+      return res.status(409).send("Course already exists");
+    }
+
+    const newCourse = await Course.create({
+      title,
+      description,
+      imageLink,
+      price,
+    });
+
+    return res
+      .status(201)
+      .json({ msg: "Course created successfully!", courseId: newCourse._id });
+  } catch (err) {
+    return res.status(500).send("Failed to create course");
+  }
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  try {
+    const courses = await Course.find({});
+    return res.status(200).json({
+      courses: courses,
+    });
+  } catch (err) {
+    return res.status(500).send("Failed to fetch courses!");
+  }
 });
 
 module.exports = router;
--- assignments-cohort/week-3/03-mongo/routes/user.js
+++ assignments-cohort/week-3/03-mongo/routes/user.js
@@ -1,22 +1,91 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({
+      username,
+      password,
+    });
+
+    if (existingUser) {
+      return res.status(409).send("User already exists");
+    }
+
+    await User.create({
+      username,
+      password,
+    });
+    return res.status(201).send("User created successfully!");
+  } catch (err) {
+    return res.status(500).send("Failed to create user");
+  }
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  try {
+    const courses = await Course.find({});
+    return res.status(200).json({
+      courses: courses,
+    });
+  } catch (err) {
+    return res.status(500).send("Failed to fetch courses!");
+  }
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  try {
+    const { courseId } = req.params;
+    const username = req.headers.username;
+
+    const course = await Course.findById(courseId);
+
+    if (!course) {
+      return res.status(404).send("Course not found");
+    }
+
+    await User.updateOne(
+      { username },
+      {
+        $push: {
+          purchasedCourses: courseId,
+        },
+      },
+    );
+
+    return res.status(200).send("Course purchased successfully!");
+  } catch (err) {
+    return res.status(500).send("Failed to purchase course");
+  }
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  try {
+    const username = req.headers.username;
+
+    const user = await User.findOne({ username });
+    const purchasedCourses = user.purchasedCourses;
+
+    const courses = await Course.find({
+      _id: {
+        $in: purchasedCourses,
+      },
+    });
+
+    return res.status(200).json({
+      courses: courses,
+    });
+  } catch (err) {
+    return res.status(500).send("Falied to fetch courses");
+  }
 });
 
-module.exports = router
+module.exports = router;
